d8d1d38b32325ce8b85e3a763455d72d8b6fb5c9
Błażej Szczygieł <spaz16@wp.pl>
2020-04-19 23:45:45 +0200

    Update MyFreeMP3 URL

--- MediaBrowser/040-MyFreeMP3.js
+++ MediaBrowser/040-MyFreeMP3.js
@@ -1,4 +1,4 @@
-var g_baseUrl = "https://myfreemp3c.com/"
+var g_baseUrl = "https://myfreemp3.best/"
 var g_url = g_baseUrl + "api"
 var g_name = "MyFreeMP3"
 var g_headers = [
@@ -14,7 +14,7 @@ var g_treeW = null
 function getInfo()
 {
     return {
-        version: 8,
+        version: 9,
         name: g_name,
         icon: ":/applications-multimedia.svgz",
     }
